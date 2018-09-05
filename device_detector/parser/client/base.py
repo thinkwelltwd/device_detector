0b255c9000be76f78b8142df494faa42f94780bd
Jerrod Martin <jerrod@compassfoundation.io>
2018-09-05 09:04:35 -0400

    Move some functions to parent class

--- device_detector/parser/client/base.py
+++ device_detector/parser/client/base.py
@@ -1,11 +1,81 @@
 from ..parser import Parser
+import string
+import re
 
 
 class BaseClientParser(Parser):
 
+    def discard_name(self) -> bool:
+        """
+        Determine if app name is of any value to us
+
+        Return True if app should be discarded
+        """
+
+        if not self.is_name_length_valid():
+            return True
+
+        if self.app_name.lower() in self.discard:
+            return True
+
+        if self.is_substring_unwanted():
+            return True
+
+        if self.unwanted_regex_match():
+            return True
+
+        return self.is_name_int()
+
+    def is_name_length_valid(self) -> bool:
+        """
+        Check if app name portion of UA is between 3 and 25 chars
+        """
+
+        if 2 < len(self.app_name) < 26:
+            return True
+
+        return False
+
+    def is_substring_unwanted(self):
+        for substring in self.unwanted_substrings:
+            if substring in self.app_name.lower(): return True
+
+    def unwanted_regex_match(self) -> bool:
+        for regex in self.remove_unwanted_regex:
+            s = re.search(regex, self.app_name, re.IGNORECASE)
+
+            if s:
+                return True
+
+        return False
+
+    def is_name_int(self) -> bool:
+        """
+        Strip punctuation from app name and return True if
+        it can be cast to int
+        """
+
+        s = self.remove_punctuation(self.app_name)
+
+        try:
+            int(s)
+            return True
+
+        except ValueError:
+            return False
+
+    @staticmethod
+    def remove_punctuation(string_with_punct: str) -> str:
+        """
+        Remove punctuation from the given string and return
+        the new string
+        """
+
+        table = str.maketrans(dict.fromkeys(string.punctuation))
+        return string_with_punct.translate(table)
+
     def dtype(self):
         return self.cache_name.lower()
-
 
 
 __all__ = (
--- device_detector/parser/client/slashed_name_extractor.py
+++ device_detector/parser/client/slashed_name_extractor.py
@@ -1,62 +1,5 @@
 from . import BaseClientParser
-import string
 import re
-
-
-# Manually update this to map app IDs to a user friendly name.
-# For example we might want to map 'yp' to 'Yellow Pages'
-# Keys should be lowercase
-NORMALIZE_ID = {
-    'accu-weather': 'AccuWeather',
-    'accuweather': 'AccuWeather',
-    'test-case': 'Test Case',
-    'httpconnection symantec': 'Symantec',
-}
-
-
-# App names that have no value to us so we want to discard them
-# Should be lowercase
-DISCARD = {
-    'product_name',
-    'null',
-    '4d531b86',
-    '4d531b9a',
-    'httppostlib',
-    'mozilla',
-    'sm-g900p-android',
-    'sm-j320p-android',
-    'mobile_ios'
-}
-
-
-# List of substrings that if found in the app name, we will
-# discard the entire app name
-# Should be lowercase
-UNWANTED_SUBSTRINGS = [
-    'ab_1.1.3011',
-    'deviceid=',
-    'timezone=',
-]
-
-
-# Regexes that we use to remove unwanted app names
-REMOVE_UNWANTED_REGEX = [
-    r'sm-\w+-android',
-    r'^4d531b',
-    r'^com\.'
-]
-
-
-# Regexes that we use to parse UA's with a similar structure
-PARSE_GENERIC_REGEX = [
-    (re.compile(r'([\w ]+)\(unknown version\) cfnetwork$', re.IGNORECASE), 1),
-    (re.compile(r'^(fbiossdk)', re.IGNORECASE), 1),
-    (re.compile(r'^samsung [\w-]+ (\w+)', re.IGNORECASE), 1),
-    (re.compile(r'(pubnub)-csharp', re.IGNORECASE), 1),
-    (re.compile(r'(microsoft office)$', re.IGNORECASE), 1),
-    (re.compile(r'^(windows assistant)', re.IGNORECASE), 1),
-    (re.compile(r'^(liveupdateengine)', re.IGNORECASE), 1),
-]
 
 
 class SlashedNameExtractor(BaseClientParser):
@@ -67,6 +10,63 @@ class SlashedNameExtractor(BaseClientParser):
     e.g. <app name>/<version>
     """
 
+# -------------------------------------------------------------------
+    # Manually update this to map app IDs to a user friendly name.
+    # For example we might want to map 'yp' to 'Yellow Pages'
+    # Keys should be lowercase
+    normalize_id = {
+        'accu-weather': 'AccuWeather',
+        'accuweather': 'AccuWeather',
+        'test-case': 'Test Case',
+        'httpconnection symantec': 'Symantec',
+    }
+
+# -------------------------------------------------------------------
+    # App names that have no value to us so we want to discard them
+    # Should be lowercase
+    discard = {
+        'product_name',
+        'null',
+        '4d531b86',
+        '4d531b9a',
+        'httppostlib',
+        'mozilla',
+        'sm-g900p-android',
+        'sm-j320p-android',
+        'mobile_ios'
+    }
+
+# -------------------------------------------------------------------
+    # List of substrings that if found in the app name, we will
+    # discard the entire app name
+    # Should be lowercase
+    unwanted_substrings = [
+        'ab_1.1.3011',
+        'deviceid=',
+        'timezone=',
+    ]
+
+# -------------------------------------------------------------------
+    # Regexes that we use to remove unwanted app names
+    remove_unwanted_regex = [
+        r'sm-\w+-android',
+        r'^4d531b',
+        r'^com\.'
+    ]
+
+# -------------------------------------------------------------------
+    # Regexes that we use to parse UA's with a similar structure
+    parse_generic_regex = [
+        (re.compile(r'([\w ]+)\(unknown version\) cfnetwork$', re.IGNORECASE), 1),
+        (re.compile(r'^(fbiossdk)', re.IGNORECASE), 1),
+        (re.compile(r'^samsung [\w-]+ (\w+)', re.IGNORECASE), 1),
+        (re.compile(r'(pubnub)-csharp', re.IGNORECASE), 1),
+        (re.compile(r'(microsoft office)$', re.IGNORECASE), 1),
+        (re.compile(r'^(windows assistant)', re.IGNORECASE), 1),
+        (re.compile(r'^(liveupdateengine)', re.IGNORECASE), 1),
+    ]
+
+# -------------------------------------------------------------------
     app_name = ''
     app_version = ''
 
@@ -85,7 +85,7 @@ class SlashedNameExtractor(BaseClientParser):
         if self.discard_name():
             return
 
-        self.app_name = NORMALIZE_ID.get(
+        self.app_name = self.normalize_id.get(
             self.app_name.lower(),
             self.app_name
         )
@@ -106,73 +106,13 @@ class SlashedNameExtractor(BaseClientParser):
 
     def clean_name(self) -> None:
         """
-        Call function to extract substring name and strip
-        punctuation from app name
+        Extract substring name and remove punctuation from
+        app name.
         """
 
         self.extract_substring_name()
 
-        self.app_name = self.app_name.strip(string.punctuation)
-
-    def discard_name(self) -> bool:
-        """
-        Determine if app name is of any value to us
-
-        Return True if app should be discarded
-        """
-
-        if not self.is_name_length_valid():
-            return True
-
-        if self.app_name.lower() in DISCARD:
-            return True
-
-        if self.is_substring_unwanted():
-            return True
-
-        if self.unwanted_regex_match():
-            return True
-
-        return self.is_name_int()
-
-    def is_name_int(self) -> bool:
-        """
-        Strip punctuation from app name and return True if
-        it can be cast to int
-        """
-
-        table = str.maketrans(dict.fromkeys(string.punctuation))
-        s = self.app_name.translate(table)
-
-        try:
-            int(s)
-            return True
-
-        except ValueError:
-            return False
-
-    def is_name_length_valid(self) -> bool:
-        """
-        Check if app name portion of UA is between 3 and 25 chars
-        """
-
-        if 2 < len(self.app_name) < 26:
-            return True
-
-        return False
-
-    def is_substring_unwanted(self):
-        for substring in UNWANTED_SUBSTRINGS:
-            if substring in self.app_name.lower(): return True
-
-    def unwanted_regex_match(self) -> bool:
-        for regex in REMOVE_UNWANTED_REGEX:
-            s = re.search(regex, self.app_name, re.IGNORECASE)
-
-            if s:
-                return True
-
-        return False
+        self.app_name = self.remove_punctuation(self.app_name)
 
     def extract_substring_name(self) -> None:
         """
@@ -181,7 +121,7 @@ class SlashedNameExtractor(BaseClientParser):
         as known.
         """
 
-        for regex in PARSE_GENERIC_REGEX:
+        for regex in self.parse_generic_regex:
             m = regex[0].match(self.app_name)
 
             if m:
